08c11b7162a46861765db1b100e7e3b49fe8e482
Rahul NS <rahul9380767545@gmail.com>
2024-11-30 19:01:17 +0530

    Add arithmetic_expressions

---
+++ src/arithmetic_expressions/ArithmeticExpressions.java
@@ -0,0 +1,29 @@
+package arithmetic_expressions;
+
+public class ArithmeticExpressions {
+    public static void main(String[] args) {
+
+        int friends = 10;
+
+        // add
+        friends = friends + 1;
+        System.out.println(friends);
+
+        // sub
+        friends = friends - 3;
+        System.out.println(friends);
+
+        // multiply
+        friends = friends * 2;
+        System.out.println(friends);
+
+        // division
+        friends = friends / 2;
+        System.out.println(friends);
+
+        // modulus
+        friends = friends % 3;
+        System.out.println(friends);
+
+    }
+}
