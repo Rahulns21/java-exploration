89bf20f232a35ef317f4abe63eddb875421d5e19
Rahul NS <rahul9380767545@gmail.com>
2024-11-30 17:36:45 +0530

    Initial commit

---
+++ src/variables/VariablesExample.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class VariablesExample {
+    public static void main(String[] args) {
+
+        //assigning variables
+        int x = 123;
+        long z = 1225667755757747474L;
+        float y = 3.14f;
+        double r = 3.1234567891011129;
+        String name = "Rahul";
+
+        System.out.println("My name is: " + name);
+    }
+}
