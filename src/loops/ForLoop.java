43e4acc2072a4cee37fa34aac99c2abc44d9a3fb
Rahul NS <rahul9380767545@gmail.com>
2024-12-02 13:42:23 +0530

    Add for loop

---
+++ src/loops/ForLoop.java
@@ -0,0 +1,21 @@
+package loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+        /* for loop: executes a block of code a limited amount of times */
+
+        // this loops starts from zero and counts till 10
+        for(int i = 0; i<=10; i++) {
+            System.out.println("Count: " + i);
+        }
+
+        // Add a line break after the first loop
+        System.out.println();
+
+        // this loop counts from 10 to 0 in reverse
+        for(int i = 10; i>=0; i--) {
+            System.out.println("Reverse Count: " + i);
+        }
+    }
+}
